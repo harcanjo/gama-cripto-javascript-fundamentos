5635daae248d85db6d8e32e1fcaeec1feec2a48d
harcanjo <hfarcanjo@gmail.com>
2022-04-12 05:56:44 -0300

    synchronous programming lab

--- lab/index.js
+++ lab/index.js
@@ -1,10 +1,19 @@
+// Log
 // console.log("Hello");
 
+// Fetch
 // fetch('https://www.mercadobitcoin.net/api/BTC/day-summary/2013/6/20/')
 // console.log(fetch('https://www.mercadobitcoin.net/api/BTC/day-summary/2013/6/20/'));
-
-fetch('https://www.mercadobitcoin.net/api/BTC/day-summary/2013/6/20/')
+/*
+   fetch('https://www.mercadobitcoin.net/api/BTC/day-summary/2013/6/20/')
     .then(res => console.log(res));
-
+*/
 // 'https://reqres.in/api/users'
 // 'https://www.mercadobitcoin.net/api/BTC/day-summary/2013/6/20/'
+
+// Synchronous
+const nameMI = 'Miriam';
+const greeting = `Hello, my name is ${nameMI}!`;
+console.log(greeting);
+// "Hello, my name is Miriam!"
+
